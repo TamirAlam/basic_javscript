3946c56d93160cf6596cf844beb54f3a8535d253
Tamir Alam <tamiralam2@gmai.com>
2024-07-14 23:54:19 +0530

    day2 work is added

---
+++ day2/operator.js
@@ -0,0 +1,120 @@
+// Arithmetic operation
+// let a=10;
+// let b=20;
+// let c=a+b;
+// console.log(c);
+
+// subtract two number
+// let a=12;
+// let c=14;
+// let b=a-c;
+// console.log(b);
+
+
+
+// multiply
+// let a=12;
+// let b=10;
+// console.log(a*b);
+
+
+
+// divide
+// let a=14;
+// let b=4;
+// let c=a/b;
+// console.log(c);
+
+
+// modulos
+// let a=10;
+// let b=2;
+// let c=a%b;
+// console.log(c);
+
+//+++++++Assignment operator+++++++++++
+// let a=12;
+// a+=12;
+// console.log(a);
+
+// let a=12;
+// a-=12;
+// console.log(a);
+
+
+// let a=18;
+// let b=12;
+// if(a>b){
+  // console.log("a is greater");
+// }
+// else{
+  // console.log("b is greater");
+// }
+
+
+// let a=30;
+// let b=28;
+// if(a>=b){
+// console.log("a");
+// 
+// }
+// else{
+  // console.log("b");
+// }
+// if(a<=b){
+  // console.log("b");
+// }
+// else{
+  // console.log("a");
+// }
+
+
+// let a=14;
+// let b=12;
+// console.log(a==b);
+// console.log(typeof(a), typeof(b));
+// console.log(a===b);
+
+
+//logical operator
+// && 
+// let a=20;
+// let b=18;
+// if(a>=b && a!==b){
+// console.log(`${a} is greater than ${b}`);
+// }else{
+  // console.log("false");
+// }
+
+
+//  ||
+// let a=12;
+// let b=14;
+// if(a>b||b>a){
+  // console.log("a is greater");
+// }
+// else{
+  // console.log("b is greater");
+// }
+
+
+
+// !
+// let a=12;
+// let b=10;
+// if(!(a>b)){
+  // console.log("a is not greater than b");
+// }
+// else{
+  // console.log("a is greater than b");
+// }
+
+
+
+// ternary operator
+
+let number=12;
+let ans = number > 0 ? "The number is positive." : (number < 0 ? "The number is negative." : "The number is zero.");
+
+console.log(ans);
+
