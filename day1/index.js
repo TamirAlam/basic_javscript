a8f526f26d129ee9f07fd6463bdb53523b93b78f
Tamir Alam <tamiralam2@gmai.com>
2024-07-14 17:35:57 +0530

    day1 folder is added

---
+++ day1/index.js
@@ -0,0 +1,53 @@
+// var a=10;
+// console.log(a);
+
+// let a=12;
+// console.log(a);
+
+
+
+// const a=13;
+// console.log(a);
+
+
+// data type
+
+// let a=10;
+// let b="Tamir";
+// let c=true;
+// let d={
+  // name:"Tamir",
+  // address:"lucknow",
+  // age:21,
+// }
+// 
+// let arr=[1,2,3,4,5,6];
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(d);
+// console.log(arr);
+// console.log("--------------------------");
+// console.log(typeof(a));
+// console.log(typeof(b));
+// console.log(typeof(c));
+// console.log(typeof(d));
+// console.log(typeof(arr));
+
+
+//activity 4
+// let a=10;
+// a=12;
+// console.log(a);
+
+
+
+//activity 5
+
+// const a=10;
+// a=13;
+// console.log(a); 
+//The const keyword is used to declare variables that are immutable, meaning their values cannot be changed after they are set.
+
+
+
