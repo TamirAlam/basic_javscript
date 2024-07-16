afb3e93784b62c279449c4c36cb803201afcd6cb
Tamir Alam <tamiralam2@gmai.com>
2024-07-16 12:58:25 +0530

    day4 work is added

---
+++ day4/loop.js
@@ -0,0 +1,77 @@
+// for loop
+
+// let a=10;
+// for(let i=0;i<=a;i++){
+// console.log(i);
+// }
+
+// let num=5;
+// for(let i=1;i<=10;i++){
+// console.log(`${num} x ${i} = ${num * i}`);
+// }
+
+// while loop
+
+// let i=1;
+// let ans=0;
+// while(i<=10){
+//  ans=ans+i;
+//  i++;
+// }
+// console.log(ans);
+
+// let i=10;
+// while(i>=1){
+// console.log(i);
+// i--;
+// }
+
+// do while loop
+// let i=1;
+// do{
+//  console.log(i);
+//  i++;
+// }
+// while(i<=5);
+
+// let num=5;
+// let fact=1;
+// let i=num;
+// do{
+//  fact*=i;
+//  i--;
+// }
+// while(i>0);
+// console.log(fact);
+
+// let num=5;
+// let fact=1;
+// let i=num;
+// while(i>0){
+// fact*=i;
+// i--;
+// }
+// console.log(fact);
+
+// let a=5;
+// for(let i=1;i<=a;i++){
+// let star='';
+// for(let j=1;j<=i;j++){
+// star+='* ';
+// }
+// console.log(star);
+// }
+
+// for(let i=1;i<=10;i++){
+// if(i==5){
+// continue;
+// }
+// console.log(i);
+// }
+
+for (let i = 1; i <= 10; i++) {
+  if (i == 7) {
+    break;
+  }
+  console.log(i);
+}
