52ec8b121d1dded04541a4a202f3289c88107a1c
Tamir Alam <tamiralam2@gmai.com>
2024-07-19 18:23:39 +0530

    day6 is added

---
+++ day6/Array.js
@@ -0,0 +1,133 @@
+// function abc(){
+// let arr=[1,2,3,4,5];
+  //  
+// for(let i=0;i<arr.length;i++){
+  // console.log(arr[i]);
+// }
+// 
+// }
+// abc();
+
+
+
+// task 2
+// function abcd(){
+  // let arr=[1,2,3,4,5,6];
+  // console.log(arr[0]+" "+arr[arr.length-1]);
+// }
+// abcd();
+
+
+
+// task3
+// function method(){
+  // let arr=[1,2,34,5,12];
+  // console.log(arr);
+  // arr.push(13);
+  // console.log(arr);
+// }
+// method();
+
+
+
+// tassk4
+// function remove(){
+  // let arr=[1,2,3,4,5,6,7];
+  // console.log(arr);
+  // console.log("++++++++++++++++++++++++++++++++++++");
+  // arr.pop();
+  // console.log(arr);
+// }
+// remove();
+
+
+
+// task5
+// function method_shift(){
+  // let arr=[12,3,4,5,6,7];
+  // console.log(arr);
+  // console.log("++++++++++");
+  // arr.shift();// remove element starting index
+  // console.log(arr);
+// }
+// method_shift();
+
+
+//  task 6
+// function another(){
+  // let arr=[1,2,3,3,4,4,4];
+  // console.log(arr);
+  // arr.unshift("tamir");
+  // console.log(arr);
+// }
+// another();
+
+
+// task7
+// use map method
+// let arr=[1,2,3,4,5,6];
+// 
+// const double=arr.map(function(num){
+// return 2*num
+// });
+// console.log(double);
+
+
+
+
+// task8
+// let arr=[1,2,3,4,5,6,7,8,10];
+// const evennumber=arr.filter(function(num){
+  // return num%2===0;
+// });
+// console.log(evennumber);
+
+
+// task9
+// let arr=[12,3,3,4,4,5,5,5];
+// const sum=arr.reduce(function(ans,curr){
+//  return ans+curr;
+// });
+// console.log(sum);
+
+
+// task 10
+// function run(){
+  // let arr=[1,2,3,3,4.5,"tamir"];
+  // for(let i=0;i<arr.length;i++){
+  //  
+  // }
+  // console.log(arr);
+// }
+// run();
+
+
+
+// task 11
+// let arr=[1,2,3,4,49,0];
+// arr.forEach(function(n){
+  // console.log(n);
+// });
+
+
+//task 12
+// let arr=[
+  // [1,2,3,4],
+  // [5,6,7,8],
+  // [9,10,11,12],
+  // [13,14,15,16]
+// ]
+// console.log(arr);
+
+
+// task13
+// const arr = [
+  // [1, 2, 3],
+  // [4, 5, 6],
+  // [7, 8, 9]
+// ];
+// console.log(arr[0][1]);
+// console.log(arr[1][2]);
+// console.log(arr[2][1]);
+
+
